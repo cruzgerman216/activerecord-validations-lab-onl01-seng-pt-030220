49bea67e6ca03a7a31bd84eda6892b14aa9cc464
German Cruz <cruzgerman216@yahoo.com>
2020-07-23 05:42:35 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
-  validates_with ClickBaitValidator
+  validates_with ClickbaitValidator
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
