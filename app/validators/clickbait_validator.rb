0a6060b9c35d4760445d688803e8d31fa42dad92
German Cruz <cruzgerman216@yahoo.com>
2020-07-23 05:46:33 +0000

    Automatically backed up by Learn

--- app/validators/clickbait_validator.rb
+++ app/validators/clickbait_validator.rb
@@ -1,6 +1,6 @@
 class ClickbaitValidator < ActiveModel::Validator
   def validate(record)
-    puts record.title?("test")
+    puts record.title.include?("test")
     unless record.title.include?("Won't Believe")
       record.errors[:title] << "We're only allowed to have people who work for the company in the database!"
       puts "test"
