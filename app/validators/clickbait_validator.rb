593ab1e7301d61d8b80231cfe5c714c636474275
German Cruz <cruzgerman216@yahoo.com>
2020-07-23 05:51:54 +0000

    Automatically backed up by Learn

--- app/validators/clickbait_validator.rb
+++ app/validators/clickbait_validator.rb
@@ -2,7 +2,7 @@ class ClickbaitValidator < ActiveModel::Validator
   def validate(record)
     puts record.title.include?("test")
     unless !record.title.include?("Won't Believe")
-      record.errors[:title] << ""
+      record.errors[:title] << "Need validation."
     end
   end
 end
