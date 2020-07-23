e6fdafc1b4bc0410a1051609f39e8e132901addb
German Cruz <cruzgerman216@yahoo.com>
2020-07-23 05:40:03 +0000

    Automatically backed up by Learn

--- app/validators/clickbait_validator.rb
+++ app/validators/clickbait_validator.rb
@@ -1,7 +1,7 @@
 class ClickBait < ActiveModel::Validator
-  def validate(post)
-    unless post.match?(/flatironschool.com/)
-      record.errors[:email] << "We're only allowed to have people who work for the company in the database!"
+  def validate(record)
+    unless record.title.match?(/flatironschool.com/)
+      record.errors[:title] << "We're only allowed to have people who work for the company in the database!"
     end
   end
 end
